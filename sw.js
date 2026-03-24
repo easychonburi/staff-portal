a91c2e579f691345194072356338178187b65593
easychonburi <sukihongyokchonburi@gmail.com>
2026-03-25 02:14:35 +0700

    Bypass POST requests in service worker
    Allow POST requests to pass through without modification to fix a bug related to unsaved data.

--- sw.js
+++ sw.js
@@ -7,6 +7,9 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  // ✅ POST request ให้ผ่านตรงๆ ไม่แตะเลย (แก้บั๊กลงเวลาไม่บันทึก)
+  if (event.request.method === 'POST') return;
+
   // โค้ดผ่านทาง เพื่อให้มือถือรู้ว่านี่คือ PWA (แอปพลิเคชัน)
   event.respondWith(fetch(event.request).catch(() => new Response('Network error')));
 });
